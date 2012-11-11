c391cf7d89840e39a319c701a88175717d949296
Eike Kettner <eike.kettner@gmail.com>
2012-11-11 19:58:21 +0100

    adopt to new publet version

--- src/main/scala/__groupId__/__ProjectName__Module.scala
+++ src/main/scala/__groupId__/__ProjectName__Module.scala
@@ -2,11 +2,12 @@ package __groupId__
 
 import com.google.inject.AbstractModule
 import org.eknet.publet.web.guice.{PubletModule, PubletBinding}
+import org.eknet.guice.squire.SquireModule
 
-class __ProjectName__Module extends AbstractModule with PubletBinding with PubletModule {
+class __ProjectName__Module extends SquireModule with PubletBinding with PubletModule {
 
   def configure() {
-    binder.bindEagerly[__ProjectName__Setup]
+    bind[__ProjectName__Setup].asEagerSingleton()
   }
 
 }
